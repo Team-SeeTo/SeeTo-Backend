ddd89916f835188e5277b051048814b8137eb74d
Lewis Kim (Sunghyun Kim) <kshr2d2@gmail.com>
2018-07-03 08:29:29 +0900

    UPDATE: DB column type from ObjectID filed to Reference field for safety

--- Server/app/models/idea.py
+++ Server/app/models/idea.py
@@ -1,18 +1,20 @@
 from mongoengine import *
 from datetime import datetime
+from .user import User
 
 
 class Comment(EmbeddedDocument):
-    author = ObjectIdField(required=True)
+    author = ReferenceField(document_type=User, required=True)
+    body = StringField(required=True)
 
 
 class Idea(Document):
     meta = {'collection': 'idea'}
 
-    author = ObjectIdField(required=True)
+    author = ReferenceField(document_type=User)
     title = StringField(required=True)
     body = StringField(required=True)
     created_at = DateTimeField(required=True, default=datetime.now())
     point = IntField(required=True, default=10)
-    upvoters = ListField(ObjectIdField, required=True, default=[])
+    upvoter = ListField(ReferenceField(document_type=User), required=True, default=[])
     comments = EmbeddedDocumentListField(Comment, default=[])
