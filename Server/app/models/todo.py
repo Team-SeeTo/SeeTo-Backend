3ea562d546525016da75f3bf1cb56c6c9baea525
Lewis Kim (Sunghyun Kim) <kshr2d2@gmail.com>
2018-07-03 21:40:35 +0900

    ADD: complete check column

--- Server/app/models/todo.py
+++ Server/app/models/todo.py
@@ -23,6 +23,7 @@ class ToDo(Document):
     type = IntEnumField(TypeEnum, required=True)
     created_at = DateTimeField(required=True, default=datetime.now())
     point = IntField(required=True, default=10)
+    is_completed = BooleanField(required=True, default=False)
 
 
 class ToDoStandard(ToDo):
