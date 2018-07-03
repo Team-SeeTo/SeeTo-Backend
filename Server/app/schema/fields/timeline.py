07fb1bcefc44a051a2a316cb215bb0376527dfc8
Lewis Kim (Sunghyun Kim) <kshr2d2@gmail.com>
2018-07-03 21:41:27 +0900

    STRUCT: additional field for timeline field

--- Server/app/schema/fields/timeline.py
+++ Server/app/schema/fields/timeline.py
@@ -1,5 +1,13 @@
 import graphene
 
 
+class ToDoReviewField(graphene.ObjectType):
+    pass
+
+
+class IdeasReviewField(graphene.ObjectType):
+    pass
+
+
 class TimeLimeField(graphene.ObjectType):
     pass
