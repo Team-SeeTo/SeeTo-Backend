725f6cea8f22b6869c1f90f16e35d45435a3d474
Lewis Kim (Sunghyun Kim) <kshr2d2@gmail.com>
2018-07-03 16:50:46 +0900

    IMPLEMENT: profile field

--- Server/app/schema/fields/profile.py
+++ Server/app/schema/fields/profile.py
@@ -2,4 +2,9 @@ import graphene
 
 
 class ProfileField(graphene.ObjectType):
-    pass
+    profilepic = graphene.String()
+    email = graphene.String()
+    username = graphene.String()
+    rank = graphene.Int()
+    point = graphene.Int()
+    register_on = graphene.String()
