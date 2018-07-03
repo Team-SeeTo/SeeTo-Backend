ea3ca4eb65276b96e993d589db1a81bb83dd7d84
Lewis Kim (Sunghyun Kim) <kshr2d2@gmail.com>
2018-07-03 22:11:19 +0900

    IMPLEMENT: field for Timeline feature

--- Server/app/schema/fields/timeline.py
+++ Server/app/schema/fields/timeline.py
@@ -2,12 +2,20 @@ import graphene
 
 
 class ToDoReviewField(graphene.ObjectType):
-    pass
+    new_create = graphene.Int()
+    milestone_complete = graphene.Int()
+    todo_complete = graphene.Int()
+    total = graphene.Int()
 
 
 class IdeasReviewField(graphene.ObjectType):
-    pass
+    new_vote = graphene.Int()
+    new_comment = graphene.Int()
+    new_create = graphene.Int()
+    total = graphene.Int()
 
 
 class TimeLimeField(graphene.ObjectType):
-    pass
+    date = graphene.DateTime
+    todo = graphene.Field(ToDoReviewField)
+    ideas = graphene.Field(IdeasReviewField)
