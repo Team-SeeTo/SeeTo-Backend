e53a5fe37b8db8e9146d0be8ee844b3b2d1df2db
Lewis Kim (Sunghyun Kim) <kshr2d2@gmail.com>
2018-07-04 16:53:09 +0900

    IMPLEMENT: field for mirror feature

--- Server/app/schema/fields/mirror.py
+++ Server/app/schema/fields/mirror.py
@@ -1,5 +1,14 @@
 import graphene
 
 
+class Growth(graphene.ObjectType):
+    todo_growth_by_point = graphene.Int()
+    ideas_growth_by_point = graphene.Int()
+    total_point_growth_by_point = graphene.Int()
+
+
 class MirrorViewField(graphene.ObjectType):
-    pass
+    month_growth = graphene.Field(Growth)
+    week_growth = graphene.Field(Growth)
+    year_growth = graphene.Field(Growth)
+    # 상세 TimeLine 비교는 TimeLine 쿼리 사용
