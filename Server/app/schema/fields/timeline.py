e72133dd394edce7a6970d560d3528b638bffd31
Lewis Kim (Sunghyun Kim) <kshr2d2@gmail.com>
2018-07-18 22:01:37 +0900

    UPDATE

--- Server/app/schema/fields/timeline.py
+++ Server/app/schema/fields/timeline.py
@@ -5,17 +5,18 @@ class ToDoReviewField(graphene.ObjectType):
     new_create = graphene.Int()
     milestone_complete = graphene.Int()
     todo_complete = graphene.Int()
-    total = graphene.Int()
+    total_point = graphene.Int()
 
 
 class IdeasReviewField(graphene.ObjectType):
     new_vote = graphene.Int()
     new_comment = graphene.Int()
     new_create = graphene.Int()
-    total = graphene.Int()
+    total_point = graphene.Int()
 
 
 class TimeLimeField(graphene.ObjectType):
-    date = graphene.DateTime
+    date = graphene.DateTime()
     todo = graphene.Field(ToDoReviewField)
     ideas = graphene.Field(IdeasReviewField)
+    total_point = graphene.Int()
