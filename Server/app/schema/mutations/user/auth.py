2e344836dbc8d5bb59015d9724905ae3b6af40f9
Lewis Kim (Sunghyun Kim) <kshr2d2@gmail.com>
2018-08-07 20:05:02 +0900

    UPDATE: auth mutation

---
+++ Server/app/schema/mutations/user/auth.py
@@ -0,0 +1,56 @@
+from uuid import uuid4
+
+import graphene
+from flask_graphql_auth import create_access_token, create_refresh_token, get_jwt_identity
+
+from app import blacklist, refresh_required
+from app.models import User
+
+
+class AuthMutation(graphene.Mutation):
+
+    class Arguments(object):
+        email = graphene.String()
+        password = graphene.String()
+
+    access_token = graphene.String()
+    refresh_token = graphene.String()
+    message = graphene.String()
+
+    def mutate(self, info, **kwargs):
+        user = User.objects(**kwargs).first()
+
+        if user is not None:
+            access_token = create_access_token(identity=kwargs["id"])
+            refresh_token = create_refresh_token(identity=str(uuid4()))
+
+            return AuthMutation(access_token=access_token, refresh_token=refresh_token, message="Login Success")
+        else:
+            return AuthMutation(message="Login failed")
+
+
+class RefreshMutation(graphene.Mutation):
+
+    class Arguments(object):
+        refresh_token = graphene.String()
+
+    access_token = graphene.String()
+    message = graphene.String()
+
+    @refresh_required
+    def mutate(self, info, refresh_token):
+        return RefreshMutation(acces_token=create_access_token(get_jwt_identity()), message="Refresh success")
+
+
+class LogoutMutation(graphene.Mutation):
+
+    class Arguments(object):
+        refresh_token = graphene.String()
+
+    is_success = graphene.Boolean()
+    message = graphene.String()
+
+    @refresh_required
+    def mutate(self, info, refresh_token):
+        blacklist.add(get_jwt_identity())
+        return LogoutMutation(is_success=True, message="Logout successful")
