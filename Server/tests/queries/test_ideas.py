df10e18a043ca3dd6885d78b3e984124d50d8f20
Lewis Kim (Sunghyun Kim) <kshr2d2@gmail.com>
2018-08-12 19:55:52 +0900

    ADD: ideas TC

---
+++ Server/tests/queries/test_ideas.py
@@ -0,0 +1,25 @@
+from tests import BasicTestCase
+import datetime
+
+
+class TestIdeas(BasicTestCase):
+    def test_ideas(self):
+        response = self.request(type="query",
+                     call='ideas(token: "{0}")'.format(self.access_token),
+                     body='''
+                     author
+                     title
+                     body
+                     createdAt
+                     upvoter
+                     comments{
+                        author
+                        body
+                     }
+                     ''')
+
+        self.assertEqual(type(response['ideas'][0]['author']), str)
+        self.assertEqual(type(response['ideas'][0]['title']), str)
+        self.assertEqual(type(response['ideas'][0]['body']), str)
+        self.assertEqual(type(response['ideas'][0]['upvoter']), list)
+        self.assertEqual(type(response['ideas'][0]['comments']), list)
