113d4e3994c294638cf735094c452dde0f910438
Lewis Kim (Sunghyun Kim) <kshr2d2@gmail.com>
2018-08-12 20:48:37 +0900

    ADD: todo TC

---
+++ Server/tests/queries/test_todo.py
@@ -0,0 +1,24 @@
+from tests import BasicTestCase
+
+
+class TestToDo(BasicTestCase):
+    def test_todo(self):
+        response = self.request(type="query",
+                                call='todo(token: "{0}")'.format(self.access_token),
+                                body='''
+                                title
+                                type
+                                createdAt
+                                milestones{
+                                    name
+                                    isCompleted
+                                }
+                                expiration
+                                isCompleted
+                                ''')
+
+        self.assertEqual(type(response['todo'][0]['type']), str)
+        self.assertEqual(type(response['todo'][0]['title']), str)
+        self.assertEqual(type(response['todo'][0]['milestones']), list)
+        self.assertEqual(type(response['todo'][0]['isCompleted']), bool)
+
