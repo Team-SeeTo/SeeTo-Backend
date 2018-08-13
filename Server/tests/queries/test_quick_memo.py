c74929358bf7b565aff66b42a818a2354726204c
Lewis Kim (Sunghyun Kim) <kshr2d2@gmail.com>
2018-08-13 12:56:39 +0900

    ADD: quickmemo TC

---
+++ Server/tests/queries/test_quick_memo.py
@@ -0,0 +1,14 @@
+from tests import BasicTestCase
+
+
+class TestToDo(BasicTestCase):
+    def test_todo(self):
+        response = self.request(type="query",
+                                call='quickMemo(token: "{0}")'.format(self.access_token),
+                                body='''
+                                title
+                                body
+                                ''')
+
+        self.assertEqual(type(response['quickMemo'][0]['body']), str)
+        self.assertEqual(type(response['quickMemo'][0]['title']), str)
