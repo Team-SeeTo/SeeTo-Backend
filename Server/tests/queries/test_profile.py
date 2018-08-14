5d3099dc65bdbb5bb295eb3152e7fd1c830efe66
Lewis Kim (Sunghyun Kim) <kshr2d2@gmail.com>
2018-08-14 13:59:49 +0900

    ADD: profile TC

---
+++ Server/tests/queries/test_profile.py
@@ -0,0 +1,21 @@
+from tests import BasicTestCase
+
+
+class TestProfile(BasicTestCase):
+    def test_profile(self):
+        response = self.request(type="query",
+                                call='profile(token :"{0}")'.format(self.access_token),
+                                body='''
+                                profilepic
+                                email
+                                username
+                                rank
+                                point
+                                registerOn
+                                ''')
+
+        self.assertEqual(type(response['profile']['email']), str)
+        self.assertEqual(type(response['profile']['username']), str)
+        self.assertEqual(type(response['profile']['rank']), int)
+        self.assertEqual(type(response['profile']['point']), int)
+
