bbf6bf90deed53cffaea833e083ee75fe108d04b
Lewis Kim (Sunghyun Kim) <kshr2d2@gmail.com>
2018-08-14 15:24:21 +0900

    ADD: leaderboards TC

---
+++ Server/tests/queries/test_leaderboards.py
@@ -0,0 +1,17 @@
+from tests import BasicTestCase
+
+
+class TestLeaderboards(BasicTestCase):
+    def test_leaderboards(self):
+        response = self.request(type="query",
+                                call='leaderboards(token :"{0}")'.format(self.access_token),
+                                body='''
+                                rank
+                                name
+                                point
+                                ''')
+
+        for user in response['leaderboards']:
+            self.assertEqual(type(user['rank']), int)
+            self.assertEqual(type(user['name']), str)
+            self.assertEqual(type(user['point']), int)
