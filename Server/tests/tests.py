31a199adb2fda855147843b6250115d6e6ddd1df
Lewis Kim (Sunghyun Kim) <kshr2d2@gmail.com>
2018-08-15 13:14:50 +0900

    UPDATE: test runner

---
+++ Server/tests/tests.py
@@ -0,0 +1,10 @@
+from .mutations.user.test_register import *
+from .mutations.user.test_auth import *
+
+from .queries.test_profile import *
+from .queries.test_ideas import *
+from .queries.test_leaderboards import *
+from .queries.test_mirror import *
+from .queries.test_quick_memo import *
+from .queries.test_timeline import *
+from .queries.test_todo import *
