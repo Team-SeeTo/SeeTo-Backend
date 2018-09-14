13789c9132f96e57af61574177f4a67061e3b612
Lewis <artoria@artoria.us>
2018-09-14 21:24:55 +0900

    IMPLEMENT: ideas resolver

--- Server/app/schema/queries/ideas.py
+++ Server/app/schema/queries/ideas.py
@@ -1,16 +1,24 @@
 from app.schema.fields import IdeasField, CommentField
-from utils import constructor
+from flask_graphql_auth import get_jwt_identity
+
 from app.schema.utils import auth_required
+from app.models import *
+from app.schema.fields import *
 
-
-from datetime import datetime
 
 @auth_required
 def resolve_ideas(root, info, **kwargs):
-    return [IdeasField(author="Lewis",
-                       title="Title",
-                       body="Body",
-                       created_at=datetime.now(),
-                       upvoter=["steve", 'david'],
-                       comments=[CommentField(author="steve",
-                                             body="Great")])]
+
+    search = kwargs.get('search_string', None)
+    filter_by = kwargs.get('filter_by', None)
+    start_rank = kwargs.get('start_rank', None) - 1
+
+    ideas = Idea.objects[start_rank: start_rank+30].order_by(Idea.point)
+
+    if filter_by is not None:
+        ideas = [idea for idea in ideas if idea.category == filter_by]
+
+    if search is not None:
+        ideas = [idea for idea in ideas if (search in idea.body) or (search in idea.title)]
+
+    return ideas
