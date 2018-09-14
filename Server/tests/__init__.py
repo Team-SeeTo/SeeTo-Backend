4d1e01609d5d32defd40a0e9ff302b6388eb5f14
Lewis <artoria@artoria.us>
2018-09-14 21:24:27 +0900

    hotfix

--- Server/tests/__init__.py
+++ Server/tests/__init__.py
@@ -29,18 +29,6 @@ class BasicTestCase(unittest.TestCase):
 
     def _create_fake_data(self):
 
-        fake_user_1 = User(email="test@seeto.services",
-                           username="test1",
-                           password="admin1234",
-                           rank=1,
-                           point=100,
-                           quick_memo=[fake_memo],
-                           todo=[fake_todo],
-                           ideas=[fake_idea],
-                           my_items=[fake_item],
-                           img_path="seeto.services/static/test1.jpg"
-                           )
-
         fake_user_2 = User(email="test2@seeto.services",
                            username="test2",
                            password="admin1234",
@@ -49,12 +37,11 @@ class BasicTestCase(unittest.TestCase):
                            img_path="seeto.services/static/test2.jpg"
                            )
 
-        fake_user_1.save()
         fake_user_2.save()
 
         fake_idea = Idea(title="title",
                          body="body",
-                         author=fake_user_1)
+                         author=fake_user_2)
 
         fake_memo = QuickMemo(title="title",
                               body="body")
@@ -72,6 +59,20 @@ class BasicTestCase(unittest.TestCase):
         fake_item.save()
         fake_memo.save()
         fake_todo.save()
+
+        fake_user_1 = User(email="test@seeto.services",
+                           username="test1",
+                           password="admin1234",
+                           rank=1,
+                           point=100,
+                           quick_memo=[fake_memo],
+                           todo=[fake_todo],
+                           ideas=[fake_idea],
+                           my_items=[fake_item],
+                           img_path="seeto.services/static/test1.jpg"
+                           )
+
+        fake_user_1.save()
 
     def _get_tokens(self):
         response = self.request(type="mutation",
