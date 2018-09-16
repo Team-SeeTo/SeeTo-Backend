b42c3374ce1e3bf2be1532d5471bbe82542e2728
Lewis <artoria@artoria.us>
2018-09-16 13:35:38 +0900

    IMPLEMENT: quick memo resolver

--- Server/app/models/quick_memo.py
+++ Server/app/models/quick_memo.py
@@ -9,4 +9,3 @@ class QuickMemo(Document):
     body = StringField(required=True)
     created_at = DateTimeField(required=True, default=datetime.now)
     point = IntField(required=True, default=10)
-    tag = ListField(StringField, default=[])
--- Server/app/schema/queries/quick_memo.py
+++ Server/app/schema/queries/quick_memo.py
@@ -1,8 +1,16 @@
 from app.schema.fields import QuickMemoField
-from utils import constructor
 from app.schema.utils import auth_required
+from app.models import User
+
+from flask_graphql_auth import get_jwt_identity
 
 
 @auth_required
 def resolve_quick_memo(root, info):
-    return [QuickMemoField(title="Title", body="Body")]*3
+    user = get_jwt_identity()
+    memos = list(User.objects(email=user).first().quick_memo)
+
+    memos.sort(key=lambda memo: memo.created_at)
+    memos.reverse()
+
+    return [QuickMemoField(title=m.title, body=m.body) for m in memos]
