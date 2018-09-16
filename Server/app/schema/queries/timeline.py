cd99e0abd9438b522513ffc88a21d971354f60b2
Lewis <artoria@artoria.us>
2018-09-16 16:08:01 +0900

    IMPLEMENT: timeline resolver

--- Server/app/schema/queries/timeline.py
+++ Server/app/schema/queries/timeline.py
@@ -1,18 +1,17 @@
 from app.schema.fields import TimeLimeField, ToDoReviewField, IdeasReviewField
 from app.schema.utils import auth_required
+from app.models import User, UserLog
 
-from datetime import datetime
+from flask_graphql_auth import get_jwt_identity
 
 
 @auth_required
-def resolve_timeline(root, info):
-    return TimeLimeField(date=datetime.now(),
-                         todo=ToDoReviewField(new_create=1,
-                                              milestone_complete=1,
-                                              todo_complete=1,
-                                              total_point=10),
-                         ideas=IdeasReviewField(new_vote=1,
-                                                new_comment=1,
-                                                new_create=1,
-                                                total_point=10),
-                         total_point=20)
+def resolve_timeline(root, info, date):
+    date = date().date()
+    user = User.objects(email=get_jwt_identity()).first()
+    timeline = [t for t in UserLog.objects(user=user) if t.date.date() == date][0]
+
+    return TimeLimeField(date=date,
+                         todo=ToDoReviewField(**dict(timeline.ToDo.to_mongo())),
+                         ideas=IdeasReviewField(**dict(timeline.Ideas.to_mongo())),
+                         total_point=timeline.ToDo.total_point + timeline.Ideas.total_point)
