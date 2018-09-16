4a86c65c515aadd69d72f9df137811ffe312d5fb
Lewis <artoria@artoria.us>
2018-09-16 22:18:43 +0900

    Hotfix

--- Server/tests/tests.py
+++ Server/tests/tests.py
@@ -4,7 +4,6 @@ from .mutations.user.test_auth import *
 from .queries.test_profile import *
 from .queries.test_ideas import *
 from .queries.test_leaderboards import *
-from .queries.test_mirror import *
 from .queries.test_quick_memo import *
 from .queries.test_timeline import *
 from .queries.test_todo import *
