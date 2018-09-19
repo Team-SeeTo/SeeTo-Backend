f5332b0eb803602d952ee3b1465a9c80c21cee5d
Lewis <artoria@artoria.us>
2018-09-19 23:27:45 +0900

    UPDATE: mutation unions

--- Server/app/schema/unions/mutation.py
+++ Server/app/schema/unions/mutation.py
@@ -4,23 +4,28 @@ from flask_graphql_auth import AuthInfoField
 from app.schema.fields import ResponseMessageField, AuthField, RefreshField
 
 
-class BaseUnion(graphene.Union):
+class ResponseUnion(graphene.Union):
+    class Meta:
+        types = (ResponseMessageField, AuthInfoField)
 
     @classmethod
     def resolve_type(cls, instance, info):
         return type(instance)
 
 
-class ResponseUnion(BaseUnion):
-    class Meta:
-        types = (ResponseMessageField, AuthInfoField)
-
-
-class AuthUnion(BaseUnion):
+class AuthUnion(graphene.Union):
     class Meta:
         types = (AuthField, AuthInfoField)
 
+    @classmethod
+    def resolve_type(cls, instance, info):
+        return type(instance)
 
-class RefreshUnion(BaseUnion):
+
+class RefreshUnion(graphene.Union):
     class Meta:
         types = (RefreshField, AuthInfoField)
+
+    @classmethod
+    def resolve_type(cls, instance, info):
+        return type(instance)
