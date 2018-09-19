2963ece5f81f482e4be02b6e43f82dcf122dfae2
Lewis <artoria@artoria.us>
2018-09-19 19:35:46 +0900

    IMPLEMENT: leaderboards resolver

--- Server/app/schema/queries/leaderboards.py
+++ Server/app/schema/queries/leaderboards.py
@@ -1,8 +1,9 @@
 from app.schema.fields import LeaderboardsField
 from app.schema.utils import auth_required
+from app.models import User
 
 
 @auth_required
 def resolve_leaderboards(root, info):
-    return [LeaderboardsField(rank=1, name="user1", point=30),
-            LeaderboardsField(rank=2, name="user2", point=20)]
+
+    return [LeaderboardsField(rank=i+1, name=u.username, point=u.point) for i, u in enumerate(User.objects.order_by('-point'))]
