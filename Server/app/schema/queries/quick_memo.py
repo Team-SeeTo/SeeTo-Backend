7e1e97e72e389cb710337e4b3d45733660e2cc56
Lewis <artoria@artoria.us>
2018-09-20 23:52:08 +0900

    ADD: return specific quick memo

--- Server/app/schema/queries/quick_memo.py
+++ Server/app/schema/queries/quick_memo.py
@@ -1,15 +1,26 @@
-from app.schema.fields import QuickMemoField
+from app.schema.fields import QuickMemoField, ResponseMessageField
 from app.models import User
 
 from flask_graphql_auth import get_jwt_identity, query_jwt_required
 
 
 @query_jwt_required
-def resolve_quick_memo(root, info):
+def resolve_quick_memo(root, info, **kwargs):
+    view_id = kwargs.get('view', None)
+
     user = get_jwt_identity()
     memos = list(User.objects(email=user).first().quick_memo)
 
     memos.sort(key=lambda memo: memo.created_at)
     memos.reverse()
 
-    return [QuickMemoField(title=m.title, body=m.body) for m in memos]
+    if view_id is not None:
+        memo = [memo for memo in memos if view_id == memo.id]
+
+        if memo == []:
+            return ResponseMessageField(is_success=False, message="Not found")
+
+        memo = memo[0]
+        return QuickMemoField(id=memo.id, title=memo.title, body=memo.body)
+
+    return [QuickMemoField(id=m.id,title=m.title, body=m.body) for m in memos]
