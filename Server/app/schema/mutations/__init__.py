35b2831a6484c7a4a5de550ee2ae5b9c9e9d2250
Lewis <artoria@artoria.us>
2018-09-20 22:54:02 +0900

    UPDATE: register mutation about idea to root mutation

--- Server/app/schema/mutations/__init__.py
+++ Server/app/schema/mutations/__init__.py
@@ -3,7 +3,7 @@ from app.schema.mutations.user.auth import AuthMutation, RefreshMutation
 from app.schema.mutations.user.register import RegisterMutation
 from app.schema.mutations.todo import NewToDoMutation
 from app.schema.mutations.quickmemo import NewQuickMemoMutation
-from app.schema.mutations.idea import NewIdeaMutation
+from app.schema.mutations.idea import NewIdeaMutation, NewCommentMutation, UpdateIdeaMutation, DeleteIdeaMutation
 from app.schema.mutations.store import BuyItemMutation
 
 
@@ -14,15 +14,15 @@ class Mutation(graphene.ObjectType):
 
     new_todo = NewToDoMutation.Field()
     new_idea = NewIdeaMutation.Field()
-    # new_comment = object
+    new_comment = NewCommentMutation.Field()
     new_quickmemo = NewQuickMemoMutation.Field()
 
     # edit_todo = object
-    # edit_idea = object
+    edit_idea = UpdateIdeaMutation.Field()
     # edit_quickmemo = object
 
     # delete_todo = object
-    # delete_idea = object
+    delete_idea = DeleteIdeaMutation.Field()
     # delete_comment = object
     # delete_quickmemo = object
 
