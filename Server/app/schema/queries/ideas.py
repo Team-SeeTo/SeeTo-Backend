daeacb6f35e13b472496f099c94279bf19021665
Lewis <artoria@artoria.us>
2018-09-20 23:58:17 +0900

    FIX: typo

--- Server/app/schema/queries/ideas.py
+++ Server/app/schema/queries/ideas.py
@@ -18,14 +18,14 @@ def resolve_ideas(root, info, **kwargs):
         if idea is None:
             return ResponseMessageField(is_success=False, message="Not found")
 
-        return IdeasField(id=view_id,
-                          author=idea.author.name,
+        return [IdeasField(id=view_id,
+                          author=idea.author.username,
                           title=idea.title,
                           body=idea.body,
                           created_at=idea.created_at,
                           upvoter=len([v.username for v in idea.upvoter]),
                           comments=[CommentField(author=c.author.username, body=c.body) for c in idea.comments],
-                          category=idea.category)
+                          category=idea.category)]
 
     ideas = Idea.objects[start_rank: start_rank+30].order_by('point')
 
