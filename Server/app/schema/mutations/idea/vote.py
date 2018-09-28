5ee1790683ce7fbca495946ec6361f3263fdc39c
Lewis <artoria@artoria.us>
2018-09-28 10:54:36 +0900

    ADD: idea vote mutation

---
+++ Server/app/schema/mutations/idea/vote.py
@@ -0,0 +1,36 @@
+import graphene
+from flask_graphql_auth import mutation_jwt_required, get_jwt_identity
+
+from app.models import User, Idea
+from app.schema.unions import ResponseUnion
+from app.schema.fields import ResponseMessageField
+
+
+class VoteIdeaMutation(graphene.Mutation):
+    class Arguments(object):
+        token = graphene.String()
+        id = graphene.String()
+
+    result = graphene.Field(ResponseUnion)
+
+    @classmethod
+    @mutation_jwt_required
+    def mutate(cls, _, info, id):
+        user = User.objects(email=get_jwt_identity()).first()
+
+        idea = Idea.objects(id=id).first()
+
+        if idea is None:
+            return VoteIdeaMutation(result=ResponseMessageField(is_success=False,
+                                                                message="Not found"))
+
+        try:
+            idea.update(push__upvoter=user)
+        except Exception as e:
+            return VoteIdeaMutation(result=ResponseMessageField(is_success=False,
+                                                                message=str(e)))
+
+        # TODO: User Log 남기는 기능은 함수로 따로 빼자
+
+        return VoteIdeaMutation(result=ResponseMessageField(is_success=True,
+                                                            message="Idea vote success"))
