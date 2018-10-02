a64b990c2770eeb870317469988dc5b257bc6438
Lewis <artoria@artoria.us>
2018-10-02 17:22:52 +0900

    UPDATE: todo query TC for test id field

--- Server/tests/queries/test_todo.py
+++ Server/tests/queries/test_todo.py
@@ -7,10 +7,12 @@ class TestToDo(BasicTestCase):
                                 call='todo(token: "{0}", orderBy: "type", searchString: "ti")'.format(self.access_token),
                                 body='''
                                 ... on ToDoField{
+                                id
                                 title
                                 type
                                 createdAt
                                 milestones{
+                                    id
                                     name
                                     isCompleted
                                 }
@@ -21,6 +23,9 @@ class TestToDo(BasicTestCase):
 
         self.assertEqual(type(response['todo'][0]['type']), str)
         self.assertEqual(type(response['todo'][0]['title']), str)
+        self.assertEqual(type(response['todo'][0]['id']), str)
+
         self.assertEqual(type(response['todo'][0]['milestones']), list)
+        self.assertEqual(type(response['todo'][0]['milestones'][0]['id']), str)
         self.assertEqual(type(response['todo'][0]['isCompleted']), bool)
 
