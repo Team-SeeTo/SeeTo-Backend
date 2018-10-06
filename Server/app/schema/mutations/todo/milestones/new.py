eeb5ecdebcc622b1060b2c20eebac9bf18f60e70
Lewis <artoria@artoria.us>
2018-10-06 22:09:46 +0900

    UPDATE: append milestone logic

---
+++ Server/app/schema/mutations/todo/milestones/new.py
@@ -0,0 +1,34 @@
+import graphene
+from flask_graphql_auth import mutation_jwt_required, get_jwt_identity, AuthInfoField
+
+from app.models import User, ToDo, Milestone, Type
+from app.schema.unions import ResponseUnion
+from app.schema.fields import ResponseMessageField
+
+
+class AppendMilestoneMutation(graphene.Mutation):
+    class Arguments(object):
+        token = graphene.String()
+        id = graphene.String()
+        new_milestone = graphene.String()
+
+    result = graphene.Field(ResponseUnion)
+
+    @classmethod
+    @mutation_jwt_required
+    def mutate(cls, _, info, id, new_milestone):
+        todo = ToDo.objects(id=id)
+        user = User.objects(email=get_jwt_identity(), todo=todo).first()
+
+        if user is None:
+            return AppendMilestoneMutation(ResponseMessageField(is_success=False, message="Not Found"))
+
+        try:
+            todo.update(push__milestones=Milestone(name=new_milestone))
+        except Exception as e:
+            return AppendMilestoneMutation(ResponseMessageField(is_success=False, message=str(e)))
+
+        # TODO: User Log 남기는 기능은 함수로 따로 빼자
+
+        return AppendMilestoneMutation(ResponseMessageField(is_success=True,
+                                                            message="Milestone append success"))
