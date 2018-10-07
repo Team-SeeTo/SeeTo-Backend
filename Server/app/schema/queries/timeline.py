2321bf8af6f638e7a89e1881f22a1442d180044f
Lewis <artoria@artoria.us>
2018-10-07 20:20:07 +0900

    UPDATE: timeline query

--- Server/app/schema/queries/timeline.py
+++ Server/app/schema/queries/timeline.py
@@ -8,10 +8,29 @@ from flask_graphql_auth import get_jwt_identity, query_jwt_required
 @query_jwt_required
 def resolve_timeline(root, info, date):
     date = datetime.strptime(date, "%Y-%m-%d").date()
+
     user = User.objects(email=get_jwt_identity()).first()
-    timeline = [t for t in UserLog.objects(user=user) if t.date.date() == date][0]
+    timeline = UserLog.objects(user=user, date=date).first()
+
+    if timeline is None:
+        return TimeLineField(date=date,
+                             todo=ToDoReviewField(new_create=0,
+                                                  todo_complete=0,
+                                                  milestone_complete=0,
+                                                  total_point=0),
+                             ideas=IdeasReviewField(new_create=0,
+                                                    new_vote=0,
+                                                    new_comment=0,
+                                                    total_point=0),
+                             total_point=0)
 
     return TimeLineField(date=date,
-                         todo=ToDoReviewField(**dict(timeline.ToDo.to_mongo())),
-                         ideas=IdeasReviewField(**dict(timeline.Ideas.to_mongo())),
-                         total_point=timeline.ToDo.total_point + timeline.Ideas.total_point)
+                         todo=ToDoReviewField(new_create=timeline.todo.new_create,
+                                              todo_complete=timeline.todo.todo_complete,
+                                              milestone_complete=timeline.todo.milestone_complete,
+                                              total_point=timeline.todo.total_point),
+                         ideas=IdeasReviewField(new_create=timeline.idea.new_create,
+                                                new_vote=timeline.idea.new_vote,
+                                                new_comment=timeline.idea.new_comment,
+                                                total_point=timeline.idea.total_point),
+                         total_point=timeline.todo.total_point + timeline.idea.total_point)
