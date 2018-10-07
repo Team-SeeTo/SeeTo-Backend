c686d4ffa3d71cb36f41a90abcaffc5c5c5bf934
Lewis <artoria@artoria.us>
2018-10-07 20:19:40 +0900

    FIX: point system

--- Server/app/schema/utils/activity_logger.py
+++ Server/app/schema/utils/activity_logger.py
@@ -10,11 +10,11 @@ def todo_activity_logger(user: object, type: str):
         log.save()
 
     if type == "new":
-        log.update(inc__todo__new_create=1)
+        log.update(inc__todo__new_create=1, inc__todo__total_point=50)
     elif type == "todo":
-        log.update(inc__todo__todo_complete=1)
+        log.update(inc__todo__todo_complete=1, inc__todo__total_point=70)
     elif type == "milestone":
-        log.update(inc__todo__milestone_complete=1)
+        log.update(inc__todo__milestone_complete=1, inc__todo__total_point=10)
 
 
 def idea_activity_logger(user: object, type: str):
@@ -25,8 +25,8 @@ def idea_activity_logger(user: object, type: str):
         log.save()
 
     if type == "new":
-        log.update(inc__idea__new_create=1)
+        log.update(inc__idea__new_create=1, inc__idea__total_point=50)
     elif type == "comment":
-        log.update(inc__idea__new_comment=1)
+        log.update(inc__idea__new_comment=1, inc__idea__total_point=20)
     elif type == "vote":
-        log.update(inc__idea__new_vote=1)
+        log.update(inc__idea__new_vote=1, inc__idea__total_point=10)
