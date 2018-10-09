222ce6912ce53fd2a72c7b3ba5375747f02183db
Lewis <artoria@artoria.us>
2018-10-09 20:30:41 +0900

    UPDATE: use specific date to test strptime code

--- Server/tests/queries/test_timeline.py
+++ Server/tests/queries/test_timeline.py
@@ -1,12 +1,11 @@
 from tests import BasicTestCase
-import datetime
 
 
 class TestTimeLine(BasicTestCase):
     def test_timeline(self):
         response = self.request(type="query",
-                     call='timeline(token: "{0}")'.format(self.access_token),
-                     body='''
+                                call='timeline(token: "{0}")'.format(self.access_token),
+                                body='''
                      ... on TimeLineField{
                      date
                      todo{
@@ -23,3 +22,22 @@ class TestTimeLine(BasicTestCase):
         self.assertEqual(type(response['timeline']['todo']['totalPoint']), int)
         self.assertEqual(type(response['timeline']['ideas']['totalPoint']), int)
 
+    def test_specific_timeline(self):
+        response = self.request(type="query",
+                                call='timeline(token: "{0}", date:"2018-10-08")'.format(self.access_token),
+                                body='''
+                     ... on TimeLineField{
+                     date
+                     todo{
+                        totalPoint
+                     }
+                     ideas{
+                        totalPoint
+                     }
+                     totalPoint
+                     }
+                     ''')
+
+        self.assertEqual(type(response['timeline']['totalPoint']), int)
+        self.assertEqual(type(response['timeline']['todo']['totalPoint']), int)
+        self.assertEqual(type(response['timeline']['ideas']['totalPoint']), int)
